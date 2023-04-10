bba7026ac02476e57e3f25d1c557f64c137ff2c7
Rodrigo <zoccorodrigo@gmail.com>
2023-04-10 15:30:00 -0300

    Agrega lógica de selección de cartas
    Al seleccionar jugadores, se mostrarán los elegidos.

--- src/index.js
+++ src/index.js
@@ -4,11 +4,13 @@ let jugadoresSeleccionados = 0;
 function manejarSeleccionJugador(jugador){
   if(!jugador.classList.contains("active") && jugadoresSeleccionados < 10){
     jugador.classList.add("active");
+    agregarCartaJugador(jugador);
     jugadoresSeleccionados ++;
   }
   else{
     if(jugador.classList.contains("active")){
       jugador.classList.remove("active");
+      eliminarCartaJugador(jugador);
       jugadoresSeleccionados --;
     }
   }
@@ -17,16 +19,12 @@ function manejarSeleccionJugador(jugador){
 };
 
 function actualizarTextoJugadores(){
+  const $textoJugadoresElegidos = document.querySelector("#jugadores-elegidos");
   if(jugadoresSeleccionados < maximoJugadores){
-    if(jugadoresSeleccionados < 5){
-      document.querySelector("#jugadores-elegidos").textContent = `Elige ${ 5 - jugadoresSeleccionados} titulares más`;
-    }
-    else{
-      document.querySelector("#jugadores-elegidos").textContent = `Elige ${maximoJugadores - jugadoresSeleccionados} suplentes`;
-    }
+    $textoJugadoresElegidos.textContent = `Elige ${ maximoJugadores - jugadoresSeleccionados} jugadores más`;
   }
   else{
-    document.querySelector("#jugadores-elegidos").textContent = `¿Este es tu equipo final?`;
+    $textoJugadoresElegidos.textContent = `¿Este es tu equipo final?`;
   }
 };
 
@@ -44,7 +42,7 @@ async function cargarJugadores(){
   return (await fetch ("src/jugadores.json")).json();
 };
 
-async function crearJugadores(){
+async function crearListaJugadores(){
   const $LISTA_JUGADORES = document.querySelector("#jugadores");
 
   let jugadoresData = await cargarJugadores();
@@ -64,9 +62,57 @@ async function crearJugadores(){
     $LISTA_JUGADORES.appendChild($jugador);
 
     $jugador.addEventListener("click", () => {
-      manejarSeleccionJugador($jugador)
+      manejarSeleccionJugador($jugador);
     });
   });
 }
 
-crearJugadores();
+crearListaJugadores();
+
+function agregarCartaJugador(jugador){
+  const $LISTA_TITULARES = document.querySelector("#titulares");
+  const $LISTA_SUPLENTES = document.querySelector("#suplentes");
+  const nombreJugador = jugador.dataset.nombre;
+  const nombre = nombreJugador.split(" ")[0];
+  const apellido = nombreJugador.split(" ")[1];
+
+  let $carta = document.createElement("div");
+  $carta.id = `${nombre}-${apellido}`;
+  $carta.style.width = "9rem";
+  
+  let $cartaCuerpo = document.createElement("div");
+  $cartaCuerpo.classList.add("card-body","text-center");
+
+  let $cartaTitulo = document.createElement("h5");
+  $cartaTitulo.classList.add("card-title");
+  $cartaTitulo.textContent = jugador.dataset.nombre;
+  $cartaCuerpo.appendChild($cartaTitulo);
+
+  let $cartaPrecio = document.createElement("p");
+  $cartaPrecio.classList.add("card-text");
+  $cartaPrecio.textContent = `${jugador.dataset.precio} $`;
+  $cartaCuerpo.appendChild($cartaPrecio);
+
+  let $cartaEquipo = document.createElement("p");
+  $cartaEquipo.classList.add("card-text");
+  $cartaEquipo.textContent = jugador.dataset.equipo;
+  $cartaCuerpo.appendChild($cartaEquipo);
+
+  $carta.appendChild($cartaCuerpo);
+
+  if( document.querySelectorAll("#titulares .card").length < 5){
+    $carta.classList.add("card","text-bg-success");
+    $LISTA_TITULARES.appendChild($carta);
+  }else{
+    $carta.classList.add("card","text-bg-primary");
+    $LISTA_SUPLENTES.appendChild($carta);
+  }
+};
+
+function eliminarCartaJugador(jugador){
+  let nombreJugador = jugador.dataset.nombre;
+  let nombre = nombreJugador.split(" ")[0];
+  let apellido = nombreJugador.split(" ")[1];
+  let cartaJugador = document.querySelector(`#${nombre}-${apellido}`);
+  cartaJugador.remove();
+};
